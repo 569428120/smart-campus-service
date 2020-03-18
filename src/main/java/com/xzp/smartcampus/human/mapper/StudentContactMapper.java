a6317b05d1c4cd929f4a29996e7b4d6608c788c7
xuzhipeng <569428120@qq.com>
2020-03-18 16:33:46 +0800

    Revert "Human模块：Student/Staff 代码基本成型。"
    This reverts commit 14e75c6cd769b1ef7a4d85c66094648ef46fb51a.

--- src/main/java/com/xzp/smartcampus/human/mapper/StudentContactMapper.java
+++
@@ -1,6 +0,0 @@
-package com.xzp.smartcampus.human.mapper;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.xzp.smartcampus.human.model.FuckTestModel;
-
-public interface StudentContactMapper extends BaseMapper<FuckTestModel>{
-}
--- src/main/java/com/xzp/smartcampus/system/mapper/RegionMapper.java
+++
@@ -1,9 +0,0 @@
-package com.xzp.smartcampus.system.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.xzp.smartcampus.system.model.RegionModel;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegionMapper extends BaseMapper<RegionModel> {
-}
