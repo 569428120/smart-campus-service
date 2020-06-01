89120855c0ae277490dce0b464fbb92a04ac7964
zoushanpeng <841933446@qq.com>
2020-06-01 21:13:38 +0800

    1.增加车辆记录CarMapper

---
+++ src/main/java/com/xzp/smartcampus/access_strategy/mapper/CarAccessMapper.java
@@ -0,0 +1,10 @@
+package com.xzp.smartcampus.access_strategy.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xzp.smartcampus.access_strategy.model.CarAccessRecordModel;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarAccessMapper extends BaseMapper<CarAccessRecordModel> {
+
+}
